f22d2555a114a2ad653f5c30d01d71f832f9acd2
Panawat.A <patjanawat@gmail.com>
2021-12-17 23:47:30 +0700

    Intro vscode
    test

---
+++ IntrolUI/Program.cs
@@ -0,0 +1,10 @@
+﻿using IntroLibrary;
+
+PersonModel p = new PersonModel{
+    FirstName = "King",
+    LastName = "Kong"
+};
+Console.WriteLine($"{p.FirstName } {p.LastName} is my name");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Kingkong test");
+Console.WriteLine("Done");
